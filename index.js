7b733b88b957fbac96e07bce6a25a28c0954c23d
trasku <rasku97@gmail.com>
2025-02-11 19:13:41 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,48 +1,25 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+require('dotenv').config()
 const app = express()
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-
-let persons = [
-    {
-      id: "1",
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: "2",
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: "3",
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    },
-    {
-        id: "5",
-        name: "Sahti Salttu",
-        number: "57-36557365"
-    }
-  ]
+const PORT = process.env.PORT
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
   })
+})
 
 app.get('/info', (request, response) => {
     const numberOfPersons = persons.length
@@ -88,7 +65,6 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
   })
 
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
